fd37a635396f7c544e951b717fb838f820abd0d7
Toihoccode1405 <son26dk9cntt@gmail.com>
2023-04-06 21:20:45 +0700

    Using ORM To Delete Data (Delete)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -71,6 +71,17 @@ let putCRUD = async (req, res) => {
     })
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDservice.deleteUserById(id);
+        return res.send('Delete user succeed!')
+    } else {
+        return res.send('User not found!')
+    }
+
+}
+
 module.exports = {
     getHomePageL: getHomePage,
     getAboutPage: getAboutPage,
@@ -79,4 +90,5 @@ module.exports = {
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
     putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
